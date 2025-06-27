9c22b1a3c3b455f89f26263810242fadde406136
idrmdhni <indrmdhni@gmail.com>
2025-06-27 20:46:52 +0800

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -220,7 +220,7 @@ USE_TZ = True
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  
+STATIC_ROOT = BASE_DIR / 'static'  
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'      
